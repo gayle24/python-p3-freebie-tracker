5c38708e90503a697dc32e5ce8c0d379eea6413a
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-09-10 19:36:29 +0300

    freebie tracker relationships

--- lib/models.py
+++ lib/models.py
@@ -16,14 +16,55 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    # Relationship to Freebie
+    freebies = relationship("Freebie", backref="company")
+
     def __repr__(self):
         return f'<Company {self.name}>'
+
+    def give_freebie(self, dev, item_name, value):
+        # Create a new Freebie instance associated with this company and the given dev
+        freebie = Freebie(dev=dev, company=self, item_name=item_name, value=value)
+        return freebie
+
+    @classmethod
+    def oldest_company(cls):
+        # Return the Company instance with the earliest founding year
+        return cls.query.order_by(cls.founding_year).first()
 
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name = Column(String())
+
+    # Relationship to Freebie
+    freebies = relationship("Freebie", backref="dev")
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+    def received_one(self, item_name):
+        # Check if any of the freebies associated with the dev has the specified item_name
+        return any(freebie.item_name == item_name for freebie in self.freebies)
+
+    def give_away(self, other_dev, freebie):
+        # Check if the freebie belongs to the dev who's giving it away
+        if freebie.dev == self:
+            freebie.dev = other_dev
+            return True
+        return False
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+
+    # Foreign keys
+    dev_id = Column(Integer, ForeignKey('devs.id'))
+    company_id = Column(Integer, ForeignKey('companies.id'))
+
+    def __repr__(self):
+        return f'<Freebie {self.item_name} from {self.company.name} to {self.dev.name}>'
